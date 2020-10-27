a1c5ae6c09368575b661df47aaaa597177217c36
Kathryn Reeve <67553+BinaryKitten@users.noreply.github.com>
2020-10-27 09:59:34 +0000

    adjustments - update models with ide-helper

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,12 +1,45 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Models;
 
-use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Illuminate\Database\Eloquent\Builder as EloquentBuilder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Notifications\DatabaseNotification;
+use Illuminate\Notifications\DatabaseNotificationCollection;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Support\Carbon as SupportCarbon;
 
+/**
+ * App\Models\User
+ *
+ * @property-read DatabaseNotificationCollection|DatabaseNotification[] $notifications
+ * @property-read int|null $notifications_count
+ * @method static EloquentBuilder|User newModelQuery()
+ * @method static EloquentBuilder|User newQuery()
+ * @method static EloquentBuilder|User query()
+ * @mixin \Eloquent
+ * @property int $id
+ * @property string $name
+ * @property string $email
+ * @property int $role
+ * @property SupportCarbon|null $email_verified_at
+ * @property string $password
+ * @property string|null $remember_token
+ * @property SupportCarbon|null $created_at
+ * @property SupportCarbon|null $updated_at
+ * @method static EloquentBuilder|User whereCreatedAt($value)
+ * @method static EloquentBuilder|User whereEmail($value)
+ * @method static EloquentBuilder|User whereEmailVerifiedAt($value)
+ * @method static EloquentBuilder|User whereId($value)
+ * @method static EloquentBuilder|User whereName($value)
+ * @method static EloquentBuilder|User wherePassword($value)
+ * @method static EloquentBuilder|User whereRememberToken($value)
+ * @method static EloquentBuilder|User whereRole($value)
+ * @method static EloquentBuilder|User whereUpdatedAt($value)
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
